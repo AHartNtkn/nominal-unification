3667cc44a660cabb751d354d494b02029b484157
AHartNtkn <AHartNtkn@users.noreply.github.com>
2020-02-09 22:43:25 -0700

    Fix __init__ imports.

--- nominal_unification/__init__.py
+++ nominal_unification/__init__.py
@@ -1,10 +1,10 @@
+from nominal_unification.Utils import *
 from nominal_unification.Exceptions import *
 from nominal_unification.Syntax import *
 from nominal_unification.Constraints import *
-from nominal_unification.Monad_Utils import *
 from nominal_unification.NuMachines import *
-from nominal_unification.RhoMachines import *
 from nominal_unification.DeltaMachines import *
+from nominal_unification.RhoMachines import *
 from nominal_unification.Unify import *
 
 __version__ = '0.1.0'
