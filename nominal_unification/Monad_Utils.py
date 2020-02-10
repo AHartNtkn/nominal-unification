7c2dce3fbbbfa08400c7ba9c642fd46ac189baff
AHartNtkn <AHartNtkn@users.noreply.github.com>
2020-02-09 17:16:10 -0700

    Ass misc. util functions for using state monads.

---
+++ nominal_unification/Monad_Utils.py
@@ -0,0 +1,26 @@
+from pymonad import *
+
+# foldr : (a -> b -> b) -> b -> [a] -> b
+def foldr(f, e, l):
+    if len(l) == 0:
+        return e
+    return f(l[0], foldr(f, e, l[1:]))
+
+# modify : (s -> s) -> State s ()
+def modify(f):
+    return State(lambda x: ((), f(x)))
+
+# get : State s s
+def get():
+    return State(lambda x: (x, x))
+
+# mapMU : (Foldable t, Monad m) => (a -> m b) -> t a -> m ()
+def mapMU(f, l):
+    return foldr(lambda x, y: f(x).bind(lambda _: y), State.unit(()), l)
+
+# foldM : (Foldable t, Monad m) => (b -> a -> m b) -> b -> t a -> m b
+def foldM(f, z0, xs):
+    def fp(x, k):
+        return lambda z: f(z, x).bind(k)
+    
+    return foldr(fp, State.unit, xs)(z0)
