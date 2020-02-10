613519b1192750a466fe24a0f87a0b0815abbfe0
AHartNtkn <AHartNtkn@users.noreply.github.com>
2020-02-09 17:26:51 -0700

    Reorder some tests.

--- nominal_unification/test/test_Monad_Utils.py
+++ nominal_unification/test/test_Monad_Utils.py
@@ -13,11 +13,11 @@ def test_mapMU():
     l = [3,4]
     def f(num):
         return State(lambda s: ((), s ** num))
-    assert mapMU(f,l).getState(2) == 4096
     assert mapMU(f,l).getResult(2) == ()
+    assert mapMU(f,l).getState(2) == 4096
 
 def test_foldM():
     def testf(b, a):
         return State(lambda s: (b ** a, s))
-    assert foldM(testf, 2, [3,4]).getState(20) == 20
     assert foldM(testf, 2, [3,4]).getResult(20) == 4096
+    assert foldM(testf, 2, [3,4]).getState(20) == 20
